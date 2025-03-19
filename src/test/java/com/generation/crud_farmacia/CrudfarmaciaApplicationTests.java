209b65103d1e67c7e4f3c3e828dc6b35741569bc
Gabrielly Barbosa Silva <gabriellybarbosa.dev@gmail.com>
2025-03-19 14:15:18 -0300

    :tada: Commit Inicial

---
+++ src/test/java/com/generation/crud_farmacia/CrudfarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.crud_farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudfarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
